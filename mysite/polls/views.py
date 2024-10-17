19afa634fdc5eaca460449ee62be360702ae2fb1
Szymon <szymonh2006@gmail.com>
2024-10-17 08:59:59 +0200

    cz4_poczatek

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,16 +13,11 @@ def detail(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def index(request):
-   	return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
